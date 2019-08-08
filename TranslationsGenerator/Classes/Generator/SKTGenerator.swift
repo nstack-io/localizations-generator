3ec0b94de9bb4f8058439d0c302f765c04d896e3
Bob De Kort <bogo@nodes.dk>
2019-08-08 13:12:44 +0200

    Fixed generators for newest version of TranslationManager

--- TranslationsGenerator/Classes/Generator/SKTGenerator.swift
+++ TranslationsGenerator/Classes/Generator/SKTGenerator.swift
@@ -47,11 +47,24 @@ struct SKTGenerator: Generator {
         
         // Add empty init
         modelString += "\n"
-        modelString += indent.string() + "public init() { }\n"
+        modelString += indent.string() + "public override init() { super.init() }\n"
+        
+        // Add decode
+        modelString += "\n"
+        modelString += indent.string() + "public required init(from decoder: Decoder) throws {\n"
+        indent = indent.nextLevel()
+        modelString += indent.string() + "super.init()\n"
+        modelString += indent.string() + "let container = try decoder.container(keyedBy: CodingKeys.self)\n"
+        output.mainKeys.forEach({
+            if $0.hasPrefix("_") { return }
+            modelString += indent.string() + "\($0.escaped) = try container.decodeIfPresent(\($0.uppercasedFirstLetter).self, forKey: .\($0)) ?? \($0.escaped)\n"
+        })
+        indent = indent.previousLevel()
+        modelString += indent.string() + "}\n"
         
         // Add subscript
         modelString += "\n"
-        modelString += indent.string() + "public subscript(key: String) -> LocalizableSection? {\n"
+        modelString += indent.string() + "public override subscript(key: String) -> LocalizableSection? {\n"
         indent = indent.nextLevel()
         modelString += indent.string() + "switch key {\n"
         output.mainKeys.forEach({
@@ -90,19 +103,46 @@ struct SKTGenerator: Generator {
                 subString += "public var \(subKey.escaped) = \"\"\n"
             }
             
+            // RITO - Start
+            subString += "\n"
+            subString += indent.string() + "enum CodingKeys: String, CodingKey {\n"
+            indent = indent.nextLevel()
+            
+            for subKey in value.keys {
+                if subKey.hasPrefix("_") { continue }
+                subString += indent.string() + "case \(subKey)\n"
+            }
+            
+            indent = indent.previousLevel()
+            subString += indent.string() + "}\n"
+            // RITO - End
+            
             // Add init
             subString += "\n"
-            subString += indent.string() + "public init() {\n"
+            subString += indent.string() + "public override init() {\n"
             indent = indent.nextLevel()
+            subString += indent.string() + "super.init()\n"
             for subKey in value.keys {
                 subString += indent.string() + "\(subKey.escaped) = \"\\(classNameLowerCased()).\(subKey.escaped)\"\n"
             }
             indent = indent.previousLevel()
             subString += indent.string() + "}\n"
             
+            //Add decode
+            subString += "\n"
+            subString += indent.string() + "public required init(from decoder: Decoder) throws {\n"
+            indent = indent.nextLevel()
+            subString += indent.string() + "super.init()\n"
+            subString += indent.string() + "let container = try decoder.container(keyedBy: CodingKeys.self)\n"
+            value.keys.forEach({
+                subString += indent.string() + "\($0.escaped) = try container.decodeIfPresent(String.self, forKey: .\($0)) ?? \"__\($0)\"\n"
+            })
+            indent = indent.previousLevel()
+            subString += indent.string() + "}\n"
+            
             // Add subscript
             subString += "\n"
-            subString += indent.string() + "public subscript(key: String) -> String? {\n"
+            subString += indent.string() + "public override subscript(key: String) -> String? {\n"
             indent = indent.nextLevel()
             subString += indent.string() + "return \"\"\n"
             indent = indent.previousLevel()
--- TranslationsGenerator/Classes/Generator/TGenerator.swift
+++ TranslationsGenerator/Classes/Generator/TGenerator.swift
@@ -66,23 +66,24 @@ struct TGenerator: Generator {
         
         // Add empty init
         modelString += "\n"
-        modelString += indent.string() + "public init() { }\n"
+        modelString += indent.string() + "public override init() { super.init() }\n"
         
         // Add decode
         modelString += "\n"
         modelString += indent.string() + "public required init(from decoder: Decoder) throws {\n"
         indent = indent.nextLevel()
+        modelString += indent.string() + "super.init()\n"
         modelString += indent.string() + "let container = try decoder.container(keyedBy: CodingKeys.self)\n"
         output.mainKeys.forEach({
             if $0.hasPrefix("_") { return } // skip underscored
             modelString += indent.string() + "\($0.escaped) = try container.decodeIfPresent(\($0.uppercasedFirstLetter).self, forKey: .\($0)) ?? \($0.escaped)\n"
         })
         indent = indent.previousLevel()
-        modelString += indent.string() + "}"
+        modelString += indent.string() + "}\n"
         
         // Add subscript
         modelString += "\n"
-        modelString += indent.string() + "public subscript(key: String) -> LocalizableSection? {\n"
+        modelString += indent.string() + "public override subscript(key: String) -> LocalizableSection? {\n"
         indent = indent.nextLevel()
         modelString += indent.string() + "switch key {\n"
         output.mainKeys.forEach({
@@ -137,12 +138,13 @@ struct TGenerator: Generator {
 
             // Add empty init
             subString += "\n"
-            subString += indent.string() + "public init() { }\n"
+            subString += indent.string() + "public override init() { super.init() }\n"
             
             // Add decode
             subString += "\n"
             subString += indent.string() + "public required init(from decoder: Decoder) throws {\n"
             indent = indent.nextLevel()
+            subString += indent.string() + "super.init()\n"
             subString += indent.string() + "let container = try decoder.container(keyedBy: CodingKeys.self)\n"
 
             value.keys.forEach({
@@ -155,7 +157,7 @@ struct TGenerator: Generator {
             
             // Add subscript
             subString += "\n"
-            subString += indent.string() + "public subscript(key: String) -> String? {\n"
+            subString += indent.string() + "public override subscript(key: String) -> String? {\n"
             indent = indent.nextLevel()
             subString += indent.string() + "switch key {\n"
             value.keys.forEach({ subString += indent.string() + "case CodingKeys.\($0).stringValue: return \($0.escaped)\n" })

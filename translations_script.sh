527b709e913c73ea93e418938876f98e99e2f3de
Marius Constantinescu <maco@nodes.dk>
2020-03-26 20:06:56 +0100

    Update translations_script.sh

--- translations_script.sh
+++ translations_script.sh
@@ -1,13 +1,13 @@
 TL_PROJ_ROOT_FOLDER="App"
-TL_GEN_PATH="${SRCROOT}/${TL_PROJ_ROOT_FOLDER}/Resources/NStack/nstack-translations-generator.bundle"
+TL_GEN_PATH="${SRCROOT}/${TL_PROJ_ROOT_FOLDER}/Resources/NStack/nstack-localizations-generator.bundle"
 TL_CONFIG_PATH="${SRCROOT}/${TL_PROJ_ROOT_FOLDER}/Resources/NStack/NStack.plist"
 TL_OUT_PATH="${SRCROOT}/${TL_PROJ_ROOT_FOLDER}/Resources/NStack/Translations"
 
 # Check if doing a clean build
-if test -f "${DERIVED_FILE_DIR}/TranslationsGenerator.lock"; then
-echo "Not clean build, won't fetch translations this time."
+if test -f "${DERIVED_FILE_DIR}/LocalizationsGenerator.lock"; then
+echo "Not clean build, won't fetch localizations this time."
 else
-echo "Clean build. Getting translations..."
-"${TL_GEN_PATH}/Contents/MacOS/nstack-translations-generator" -plist "${TL_CONFIG_PATH}" -output "${TL_OUT_PATH}"
-touch "${DERIVED_FILE_DIR}/TranslationsGenerator.lock" # create lock file
+echo "Clean build. Getting localizations..."
+"${TL_GEN_PATH}/Contents/MacOS/nstack-localizations-generator" -plist "${TL_CONFIG_PATH}" -output "${TL_OUT_PATH}"
+touch "${DERIVED_FILE_DIR}/LocalizationsGenerator.lock" # create lock file
 fi
